126faa8c5ffdc41bdd22a69e6768407904d2982b
LyhourChhen <ukamilove66@gmail.com>
2021-05-25 21:39:41 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    document.getElementById("root")
+  );
+};
 
+render();
+const onServiceWorkerUpdate = (): void => {
+  render();
+};
+
+if (process.env.NODE_ENV) {
+  serviceWorkerRegistration.register({
+    onSuccess: onServiceWorkerUpdate,
+    onUpdate: onServiceWorkerUpdate,
+  });
 serviceWorkerRegistration.register();
 reportWebVitals();
