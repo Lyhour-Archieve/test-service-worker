82f48cf284996929cb2a2d852522922eacddb04c
LyhourChhen <ukamilove66@gmail.com>
2021-05-26 09:27:23 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./service-worker/reportWebVitals";
 
 const render = () => {
   ReactDOM.render(
